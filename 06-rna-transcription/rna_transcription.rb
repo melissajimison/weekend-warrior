aea235adf99204395d01fa64e9611ecc875f437b
Melissa Jimison <melissajimison@gmail.com>
2016-04-15 08:44:19 -0700

    Added another way to solve last dot

--- 06-rna-transcription/rna_transcription.rb
+++ 06-rna-transcription/rna_transcription.rb
@@ -1,12 +1,11 @@
 class Complement
   def self.of_dna(protein)
     raise ArgumentError.new("Not a Valid protein") unless protein.scan(/[^CGTA]/).empty?
-
     protein.gsub(/[CGTA]/, 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U')
   end
 
   def self.of_rna(protein)
-    raise ArgumentError.new("Not a Valid protein") unless protein.scan(/[^GCUA]/).empty?
+    raise ArgumentError.new("Not a Valid protein") if protein.match(/[^GCUA]/) != nil
     protein.gsub(/[GCUA]/, 'G' => 'C', 'C' => 'G', 'U' => 'A', 'A' => 'T')
 
   end
